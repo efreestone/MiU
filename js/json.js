c2b617f2043e8a040bb216be5b90bda9766a56c7
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2012-10-22 13:54:01 -0600

    Initial MiU commit with files from VFW project 4. Class name updated.

---
+++ js/json.js
@@ -0,0 +1,35 @@
+/*
+Elijah Freestone
+VFW 1210
+Project 4
+10-14-12
+*/
+
+var json = {
+	"event1": {
+		"events": ["Event type:", "Birthday"],
+		"evdate": ["Date:", "2012-11-21"],
+		"evinfo": ["Info:", "My Birthday"],
+		"importance": ["Importance:", "5"],
+		"attend": ["Is attendance required?:", "Yes"],
+		"details": ["Event details:", "Partay!!!"],
+	},
+	
+	"event2": {
+		"events": ["Event type:", "Anniversary"],
+		"evdate": ["Date:", "2013-03-26"],
+		"evinfo": ["Info:", "1 year in school"],
+		"importance": ["Importance:", "2"],
+		"attend": ["Is attendance required?:", "No"],
+		"details": ["Event details:", "12 months down. 20 to go."],
+	},
+	
+	"event3": {
+		"events": ["Event type:", "Other"],
+		"evdate": ["Date:", "2012-12-21"],
+		"evinfo": ["Info:", "End of the world"],
+		"importance": ["Importance:", "4"],
+		"attend": ["Is attendance required?:", "Undecided"],
+		"details": ["Event details:", "This should be interesting..."],
+	}
+};
