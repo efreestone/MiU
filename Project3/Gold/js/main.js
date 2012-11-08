91883f27e2f63c1f01d4e040d5f18b1ae1e3c1b6
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2012-11-08 14:16:43 -0700

    Added pop up error dialog box

--- Project3/Gold/js/main.js
+++ Project3/Gold/js/main.js
@@ -6,15 +6,26 @@ Gold App
 11-04-12
 */
 
-$('#index').on("pageinit", function(){
+$('#index').on('pageinit', function(){
 	//code needed for home page goes here
 });	
 		
-$('#addItem').on("pageinit", function(){
+$('#addItem').on('pageinit', function(){
 
-		var myForm = $("#eventForm");
+		var myForm = $("#eventForm"),
+			popErrors = $('#popErrors');
+			
 		    myForm.validate({
 			invalidHandler: function(form, validator) {
+				popErrors.click(); //Pop up error dialog box
+				var html = '';
+				for(var key in validator.submitted) {
+					var label = $('label[for^="'+ key +'"]') // Selector search for required labels not filled out
+					var legend = label.closest('fieldset').find('ui-controlgroup-label');
+					var fieldName = legend.length ? legend.text() : label.text();
+					html += '<li>'+ fieldName +'</li>';
+				};
+				$("#formErrors ul").html(html); // Fill out Pop up error text
 			},
 			submitHandler: function() {
 		var data = myForm.serializeArray();
@@ -38,7 +49,7 @@ var getData = function(){
 };*/
 
 var storeData = function(data){
-	//console.log(data);
+	
 }; 
 /*
 var	deleteItem = function (){
