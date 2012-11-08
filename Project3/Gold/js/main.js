f8acfe254d37f6f66ed6dc851f3f0f9477293335
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2012-11-07 20:37:52 -0700

    Added validate and various small tweaks and cleanup

--- Project3/Gold/js/main.js
+++ Project3/Gold/js/main.js
@@ -6,19 +6,20 @@ Gold App
 11-04-12
 */
 
-$('#index').on('pageinit', function(){
+$('#index').on("pageinit", function(){
 	//code needed for home page goes here
 });	
 		
-/*$('#addItem').on('pageinit', function(){
+$('#addItem').on("pageinit", function(){
 
-		var myForm = $('#formId');
+		var myForm = $("#eventForm");
 		    myForm.validate({
 			invalidHandler: function(form, validator) {
 			},
 			submitHandler: function() {
 		var data = myForm.serializeArray();
 			storeData(data);
+			
 		}
 	});
 	
@@ -28,24 +29,24 @@ $('#index').on('pageinit', function(){
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
-var autofillData = function (){
+/*var autofillData = function (){
 	 
 };
 
 var getData = function(){
 
-};
+};*/
 
 var storeData = function(data){
-	
+	console.log(data);
 }; 
-
+/*
 var	deleteItem = function (){
 			
 };
 					
 var clearLocal = function(){
 
-};
+};*/
 
 
